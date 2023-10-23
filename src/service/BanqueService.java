8a42d3293cacda6c658da5fd5850c012cb3269ea
ahmed <ahmedadam.boussarsar@gmail.com>
2023-10-23 20:59:19 +0100

    Second Commit

--- src/service/BanqueService.java
+++ src/service/BanqueService.java
@@ -2,19 +2,20 @@ package service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class BanqueService {
 	List<Compte> comptes = new ArrayList<Comptes>();
-
-	public List<Compte> getComptes() {
-		return comptes;
-	}
-
-	public Compte getCompte(int x) {
-		return comptes.get(x);
-	}
-
 	public double conversion(double montant) {
-		return montant * 3.36;
+		return montant * 3.36;}
+	public Compte getCompte(int code) {
+		return new compte(code, 0, new Date());
+	}
+	public List<Compte> getComptes() {
+		Compte c1 = new Compte(0, 0, new Date());
+		Compte c2 = new Compte(1, 0, new Date());
+		comptes.add(c1);
+		comptes.add(c2);
+		return comptes;
 	}
 }
--- src/service/Compte.java
+++ src/service/Compte.java
@@ -1,38 +1,30 @@
 package service;
 
-public class Compte {
-	private int code;
-	private int solde;
-	private String dateCreation;
+import java.util.*;
 
-	public Compte(int code, int solde, String dateCreation) {
+public class Compte {
+	public Compte(int code, double solde, Date date) {
 		super();
 		this.code = code;
 		this.solde = solde;
-		this.dateCreation = dateCreation;
+		this.date = date;
 	}
-
 	public int getCode() {
 		return code;
 	}
-
 	public void setCode(int code) {
 		this.code = code;
 	}
-
 	public int getSolde() {
 		return solde;
 	}
-
 	public void setSolde(int solde) {
 		this.solde = solde;
 	}
-
-	public String getDateCreation() {
-		return dateCreation;
+	public String getDate() {
+		return date;
 	}
-
-	public void setDateCreation(String dateCreation) {
-		this.dateCreation = dateCreation;
+	public void setDate(Date date) {
+		this.date = date;
 	}
 }
